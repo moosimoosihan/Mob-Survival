a1af78e068ef757b105cb2be24b50ad20ed50e70
moosimoosihan <segun408@gmail.com>
2023-06-07 18:01:27 +0900

    아이템 획득 안되는 버그 수정

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,6 +5,7 @@ public class Item : MonoBehaviour
     public string itemName;
     public int count; // 아이템이 가진 수량
     public bool isMag = false; // 플레이어의 일정 반경에 닿았을 경우
+    bool touch = false;
 
     void Update()
     {
@@ -19,9 +20,28 @@ public class Item : MonoBehaviour
     }
     void OnDisable() {
         isMag = false;
+        touch = false;
     }
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject == GameManager.instance.playerControl.mainCharacter && other.CompareTag("Player")){
+        if(other.gameObject == GameManager.instance.playerControl.mainCharacter && other.CompareTag("Player") && !touch){
+            touch = true;
+            switch(itemName){
+                case "Exp":
+                    GameManager.instance.GetExp(count);
+                    break;
+                case "Gold":
+                    GameManager.instance.GetGold(count);
+                    break;
+                    case "Item":
+                    // 아이템 별 구현
+                    break;
+            }
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.gameObject == GameManager.instance.playerControl.mainCharacter && other.CompareTag("Player") && !touch){
+            touch = true;
             switch(itemName){
                 case "Exp":
                     GameManager.instance.GetExp(count);
