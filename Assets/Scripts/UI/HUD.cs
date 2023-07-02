c002ffcfad8dd6650fcc0731712fe90e5dbfcc80
moosimoosihan <segun408@gmail.com>
2023-07-02 21:22:35 +0900

    맵 추가 및 UI 수정

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -8,12 +8,14 @@ public class HUD : MonoBehaviour
     public int playerNum;
 
     Text myText;
+    Text myChildText;
     Slider mySlider;
     Image myImage;
 
     void Awake()
     {
         myText = GetComponent<Text>();
+        myChildText = GetComponentInChildren<Text>();
         mySlider = GetComponent<Slider>();
         myImage = GetComponent<Image>();
     }
@@ -25,6 +27,9 @@ public class HUD : MonoBehaviour
                 float curExp = GameManager.instance.exp;
                 float maxExp = GameManager.instance.nextExp[Mathf.Min(GameManager.instance.level,GameManager.instance.nextExp.Length-1)];
                 mySlider.value = curExp/maxExp;
+                if(myChildText!=null){
+                    myChildText.text = curExp.ToString() + "/" + maxExp.ToString();
+                }
                 break;
             case InfoYType.Level:
                 myText.text = string.Format("Lv.{0:F0}",GameManager.instance.level + 1);
@@ -45,12 +50,15 @@ public class HUD : MonoBehaviour
                 float curHealth = GameManager.instance.players[playerNum].curHP;
                 float maxHealth = GameManager.instance.players[playerNum].maxHP;
                 mySlider.value = curHealth/maxHealth;
+                if(myChildText!=null){
+                    myChildText.text = curHealth.ToString() + "/" + maxHealth.ToString();
+                }
                 break;
             case InfoYType.Gold:
                 myText.text = string.Format("{0:F0}",GameManager.instance.gold);
                 break;
             case InfoYType.Life:
-                myText.text = string.Format("{0:F0}",GameManager.instance.life);
+                myText.text = string.Format("x {0:F0}",GameManager.instance.life);
                 break;
             case InfoYType.ActiveSkill:
                 myImage.fillAmount = GameManager.instance.players[playerNum].GetComponentInChildren<ActiveSkill>().timer/GameManager.instance.players[playerNum].GetComponentInChildren<ActiveSkill>().delay;
@@ -60,8 +68,14 @@ public class HUD : MonoBehaviour
                     float curShield = GameManager.instance.players[playerNum].curShield;
                     float maxShield = GameManager.instance.players[playerNum].maxShield;
                     mySlider.value = curShield/maxShield;
+                    if(myChildText!=null){
+                        myChildText.text = curShield.ToString() + "/" + maxShield.ToString();
+                    }
                 } else {
                     mySlider.value = 0;
+                    if(myChildText!=null){
+                        myChildText.text = "0/0";
+                    }
                 }
                 break;
         }
