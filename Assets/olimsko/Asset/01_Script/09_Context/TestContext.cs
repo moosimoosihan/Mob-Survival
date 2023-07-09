5bb9da035a579507690200ab1012cfb827370090
olimsko <olimsko@gmail.com>
2023-07-09 22:22:05 +0900

    [fix] character key changed

--- Assets/olimsko/Asset/01_Script/09_Context/TestContext.cs
+++ Assets/olimsko/Asset/01_Script/09_Context/TestContext.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using olimsko;
+using UnityEngine.InputSystem;
+
 public class TestContext : ContextModel
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    // void Start()
+    // {
+    //     OSManager.GetService<InputManager>().GetAction("Move").Enable();
+    //     OSManager.GetService<InputManager>().GetAction("Move").performed += OnMove;
 
-    }
+    //     OSManager.GetService<InputManager>().GetAction("GatheringCharacter").Enable();
+    //     OSManager.GetService<InputManager>().GetAction("GatheringCharacter").performed += OnGather;
+    // }
 
+    // private void OnMove(InputAction.CallbackContext obj)
+    // {
+    //     obj.ReadValue<Vector2>();
+    // }
+
+    // private void OnGather(InputAction.CallbackContext obj)
+    // {
+    //     if (obj.)
+    //     {
+    //         Debug.Log("ssss");
+    //     }
+    // }
     // Update is called once per frame
     void Update()
     {
